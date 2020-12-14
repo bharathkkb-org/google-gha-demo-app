2be0df629b6bf94747321f3a2bd297f2909f1a32
Bharath KKB <bharathkrishnakb@gmail.com>
2020-12-14 12:48:29 -0800

    Update app.py (#2)

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    target = os.environ.get('TARGET', 'GitHub')
+    target = os.environ.get('TARGET', 'World')
     return render_template("index.html", content=f"Hello {target}")
 
 if __name__ == "__main__":
